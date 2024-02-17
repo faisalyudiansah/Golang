73fe9bd2e7802ef31e002dad5ccd6e3b6ff0ff8b
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-02-17 15:11:09 +0700

    feat: number basic

---
+++ basic/number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`one = `, 1)              // use ` `
+	fmt.Println("two = ", 2)              // use " " and cannot use ' '
+	fmt.Println("two point five = ", 2.5) // float
+}
